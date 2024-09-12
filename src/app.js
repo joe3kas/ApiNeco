e87a4c33e1b35c641033c0ef957540c93e0be663
joe3kas <joe3kas@gmail.com>
2024-09-12 16:18:49 -0500

    updated

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@ app.use (express.static('public'))
 app.use (rutasImages)
 
 
-app.use ((req,res) => {
-    res.status (404).json({status:false,errors: 'Not found' })
-})
+//app.use ((req,res) => {
+  //  res.status (404).json({status:false,errors: 'Not found' })
+//})
 app.get('/', (req,res) => {res. send( 'Hola mundo')} )
 
 export default app;
